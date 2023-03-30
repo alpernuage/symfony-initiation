a36dc69acca704215df7227276ab6663daabbcdc
alpernuage <50120472+alpernuage@users.noreply.github.com>
2023-03-30 16:52:27 +0200

    feat(user fixtures): create user fixtures with fakerphp (#18)
    * feat(user fixtures): create user fitures with fakerphp
    
    * refactor(user fixtures): update fixtures with optional email thas is nullable in database
    
    * Update src/DataFixtures/AppFixtures.php
    
    * refactor(appfixtures): update fixtures
    
    * Update src/DataFixtures/AppFixtures.php
    
    * refactor(user fixtures): rename file

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFirstName($faker->firstName);
+            $user->setLastName($faker->lastName);
+            if($i %2 === 0) {
+                $user->setEmail($faker->safeEmail);
+            }
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
