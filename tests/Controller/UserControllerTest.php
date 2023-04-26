f8463ab8d3d8b2f3ff9d07d6aa36e92957f6577c
Alper AKBULUT <50120472+alpernuage@users.noreply.github.com>
2023-04-26 14:51:50 +0200

    test(create user): add create user test (#40)
    * test(create user): add create user test
    
    * test(create user): add create user test
    
    * refactor(test create user): add test to see title of the create page
    
    * test(create user): add create user test with invalid email
    
    * refactor(test create user): replace comments by Given, Then, And comment concept
    
    * Update tests/Controller/UserControllerTest.php
    
    * refactor(test create user): add user not exists assertion before submit form
    
    * refactor(test create user): test that user is not created before submit form
    
    * refactor(test create user): add test with too long fields that trigger an error
    
    * refactor(test create user): use findOneByEmail method to retrieve data more simply
    
    * refactor(test create user): replace 422 code status by Response::HTTP_UNPROCESSABLE_ENTITY

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserControllerTest extends WebTestCase
 {
@@ -16,6 +17,88 @@ class UserControllerTest extends WebTestCase
         $this->assertResponseStatusCodeSame(200);
         $this->assertSelectorTextContains('h1', 'Users list');
     }
+
+    public function testCreateUser(): void
+    {
+        // Given the create user page is displayed
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/user/create');
+
+        $this->assertRouteSame('user_create');
+        $this->assertSelectorTextContains('h1', 'Create User');
+
+        // When the user clicks on "return to user list" button
+        $link = $crawler->filter('a:contains("Cancel and return to the user list")')->link();
+        $client->click($link);
+
+        // Then the user list page should be displayed
+        $this->assertSelectorTextContains('h1', 'Users list');
+        $this->assertRouteSame('user_list');
+
+        // And the user is not created yet
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $notCreatedUser = $userRepository->findOneByEmail('alper.akbulut@alximy.io');
+
+        $this->assertNull($notCreatedUser);
+        $this->assertSelectorNotExists('div:contains("New user Alper AKBULUT created with success.")');
+
+        // When the user submits the create user form with valid data
+        $client->request('GET', '/user/create');
+
+        $client->submitForm('Create User', [
+            'user_form[firstName]' => 'Alper',
+            'user_form[lastName]' => 'AKBULUT',
+            'user_form[email]' => 'alper.akbulut@alximy.io',
+        ]);
+
+        // Then the user should be redirected to the user details page
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("New user Alper AKBULUT created with success.")');
+
+        // And the newly created user details should be displayed
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertSelectorTextContains('h1', 'User Details');
+        $this->assertSelectorTextContains('td', 'Alper');
+    }
+
+    public function testCreateUserWithInvalidEmail(): void
+    {
+        // Given the create user page is displayed
+        $client = static::createClient();
+        $client->request('GET', '/user/create');
+
+        // When the user submits the create user form with INVALID data
+        $client->submitForm('Create User', [
+            'user_form[firstName]' => 'Alper',
+            'user_form[lastName]' => 'AKBULUT',
+            'user_form[email]' => 'invalid-email@test',
+        ]);
+
+        // Then an error message should be shown in the same page
+        static::assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $this->assertSelectorTextContains('h1', 'Create User');
+        $this->assertSelectorTextContains('ul li', 'This value is not a valid email address');
+    }
+
+    public function testCreateUserWithTooLongFields(): void
+    {
+        // Given the create user page is displayed
+        $client = static::createClient();
+        $client->request('GET', '/user/create');
+
+        // When the user submits the create user form with TOO LONG data
+        $client->submitForm('Create User', [
+            'user_form[firstName]' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisi sit amet lacus pulvinar ullamcorper. Etiam neque orci, feugiat nec augue a, dapibus eleifend ante. Nam congue, tellus in hendrerit cursus, lacus justo iaculis sapien, sit amet tempor elit erat in ex. Cras a ipsum sit amet fusce.',
+            'user_form[lastName]' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisi sit amet lacus pulvinar ullamcorper. Etiam neque orci, feugiat nec augue a, dapibus eleifend ante. Nam congue, tellus in hendrerit cursus, lacus justo iaculis sapien, sit amet tempor elit erat in ex. Cras a ipsum sit amet fusce.',
+            'user_form[email]' => 'valid-email@test.com',
+        ]);
+
+        // Then an error message should be shown in the same page
+        static::assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertSelectorTextContains('h1', 'Create User');
+        $this->assertSelectorTextContains('ul li', 'This value is too long. It should have 255 characters or less.');
 
     public function testShowUser(): void
     {
