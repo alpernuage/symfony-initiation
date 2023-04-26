01876ef1dfc4417c4ea29c541a351bbfc5eadba5
Alper AKBULUT <50120472+alpernuage@users.noreply.github.com>
2023-04-26 08:26:55 +0200

    test(show user): test show user details page (#38)
    * test(show user): test show user is successfull
    
    * refactor(test show user): get random user from users to have always a valid email value
    
    2- remove not needed assert
    
    * feat(test user show): add route name matching assertion
    
    * refactor(test show user): use test user for "testShowUser" method in place of findAll method
    
    it is not necessary to load all users to display a single user
    
    * refactor(test show user): add Given Whent Then comments

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,5 +15,22 @@ class UserControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(200);
         $this->assertSelectorTextContains('h1', 'Users list');
+    }
+
+    public function testShowUser(): void
+    {
+        // Given client with User Repository
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // When the user clicks on "Show User" button of a user having this email
+        $testUser = $userRepository->findOneBy(['email' => 'test.user@example.com']);
+        $client->request(Request::METHOD_GET, '/user/' . $testUser->getId());
+
+        // Then the details ot selected user should be displayed
+        $this->assertRouteSame('user_show', ['id' => $testUser->getId()]);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertSelectorTextContains('h1', 'User Details');
+        $this->assertSelectorTextContains('td', $testUser->getFirstName());
     }
 }
