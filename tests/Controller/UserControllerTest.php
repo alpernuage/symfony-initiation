bd9210caaa5a76917fedadbff5c146625f7f0435
Alper AKBULUT <50120472+alpernuage@users.noreply.github.com>
2023-05-04 09:52:15 +0200

    test(update user): add test for update user (#50)
    * feat(test update user): add test for update user
    
    * test(update with invalid email): fail test: update user with invalid email
    
    * test(fail test update user): add testUpdateUserWithTooLongFields test method
    
    * test(unique constraint): add unique email constraint test
    
    * refactor(test unique email): add missing create page call to unique constraint fail cases
    
    * test(refactor unique constraint test): remove unique constraint test on create

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -119,6 +119,127 @@ class UserControllerTest extends WebTestCase
         $this->assertSelectorTextContains('td', $testUser->getFirstName());
     }
 
+    public function testUpdateUser(): void
+    {
+        // Given client with User Repository
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // When the user clicks on "Edit User" button of a user having this email
+        $testUser = $userRepository->findOneBy(['email' => 'test.user@example.com']);
+        $client->request(Request::METHOD_GET, '/user/edit/' . $testUser->getId());
+
+        // Then the details ot selected user should be displayed in editable inputs
+        $this->assertRouteSame('user_edit', ['id' => $testUser->getId()]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('h1', 'Edit User');
+        $this->assertInputValueSame('user_form[email]', $testUser->getEmail());
+
+        // When the user submits the edit user form
+        $client->submitForm('Save', [
+            'user_form[firstName]' => 'Updated First Name',
+            'user_form[lastName]' => 'Updated Last Name',
+            'user_form[email]' => 'updated.email@example.com',
+        ]);
+
+        // Then the user should be updated in the database
+        $updatedUser = $userRepository->find($testUser->getId());
+        $this->assertEquals('Updated First Name', $updatedUser->getFirstName());
+        $this->assertEquals('Updated Last Name', $updatedUser->getLastName());
+        $this->assertEquals('updated.email@example.com', $updatedUser->getEmail());
+
+        // Then the user should be redirected to the user details page
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertRouteSame('user_show', ['id' => $testUser->getId()]);
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorExists('div:contains("User Updated First Name Updated Last Name updated with success.")');
+
+        // And html tags could be found with correct contents
+        $this->assertSelectorTextContains('h1', 'User Details');
+        $this->assertSelectorTextContains('td', 'Updated First Name');
+    }
+
+    public function testUpdateUserWithInvalidEmail(): void
+    {
+        // Given client with User Repository
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // When the user clicks on "Edit User" button of a user having this email
+        $testUser = $userRepository->findOneBy(['email' => 'test.user@example.com']);
+        $client->request(Request::METHOD_GET, '/user/edit/' . $testUser->getId());
+
+        // When the user submits the edit user form with INVALID data
+        $client->submitForm('Save', [
+            'user_form[firstName]' => 'Updated First Name',
+            'user_form[lastName]' => 'Updated Last Name',
+            'user_form[email]' => 'invalid-email@test',
+        ]);
+
+        // Then the user should not be updated in the database
+        $notUpdatedUser = $userRepository->findOneBy(['email' => 'invalid-email@test']);
+        $this->assertNull($notUpdatedUser);
+        $this->assertSelectorNotExists('div:contains("User Updated First Name Updated Last Name updated with success.")');
+
+        // Then an error message should be shown in the same page
+        static::assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $this->assertSelectorTextContains('h1', 'Edit User');
+        $this->assertSelectorTextContains('ul li', 'This value is not a valid email address');
+    }
+
+    public function testUpdateUserWithTooLongFields(): void
+    {
+        // Given client with User Repository
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // When the user clicks on "Edit User" button of a user having this email
+        $testUser = $userRepository->findOneBy(['email' => 'test.user@example.com']);
+        $client->request(Request::METHOD_GET, '/user/edit/' . $testUser->getId());
+
+        // When the user submits the edit user form with TOO LONG data
+        $client->submitForm('Save', [
+            'user_form[firstName]' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisi sit amet lacus pulvinar ullamcorper. Etiam neque orci, feugiat nec augue a, dapibus eleifend ante. Nam congue, tellus in hendrerit cursus, lacus justo iaculis sapien, sit amet tempor elit erat in ex. Cras a ipsum sit amet fusce.',
+            'user_form[lastName]' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisi sit amet lacus pulvinar ullamcorper. Etiam neque orci, feugiat nec augue a, dapibus eleifend ante. Nam congue, tellus in hendrerit cursus, lacus justo iaculis sapien, sit amet tempor elit erat in ex. Cras a ipsum sit amet fusce.',
+            'user_form[email]' => 'valid-email@test.com',
+        ]);
+
+        // Then the user should not be updated in the database
+        $notUpdatedUser = $userRepository->findOneBy(['email' => 'valid-email@test.comt']);
+        $this->assertNull($notUpdatedUser);
+
+        // Then an error message should be shown in the same page
+        static::assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $this->assertSelectorTextContains('h1', 'Edit User');
+        $this->assertSelectorTextContains('ul li', 'This value is too long. It should have 255 characters or less.');
+    }
+
+    public function testUniqueEmailConstraintOnUpdate(): void
+    {
+        // Given user repository
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // Then find a user existing in database
+        $user = $userRepository->findOneBy(['email' => 'test.user@example.com']);
+        $this->assertNotNull($user);
+        $this->assertInstanceOf(User::class, $user);
+
+        // Then a user is updated with an EXISTING email
+        $client->request(Request::METHOD_GET, '/user/edit/' . $user->getId());
+        $client->submitForm('Save', [
+            'user_form[firstName]' => 'Updated First Name',
+            'user_form[lastName]' => 'Updated Last Name',
+            'user_form[email]' => 'test.user@example.com',
+        ]);
+
+        // Then the UPDATE FAILS and an error message displays
+        $this->assertSelectorTextContains('ul li', 'The value "test.user@example.com" is already used');
+    }
+    
     public function testUniqueEmailConstraintOnCreate(): void
     {
         // Given user repository
