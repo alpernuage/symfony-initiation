59b2f9f909ced30b594da46a8b02dcd2cd6d21c7
Alper AKBULUT <50120472+alpernuage@users.noreply.github.com>
2023-05-23 09:20:58 +0200

    feat(show home details): add show details page and show_route (#70)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Domain\PaginationTrait;
+use App\Entity\Home;
 use App\Repository\HomeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 final class HomeController extends AbstractController
 {
@@ -28,6 +30,14 @@ final class HomeController extends AbstractController
             'current_page' => $paginationData['current_page'],
             'pages_count' => $paginationData['pages_count'],
             'limit' => $paginationData['limit'],
+        ]);
+    }
+
+    #[Route('/home/{id}', name: 'home_show', requirements: ['id' => Requirement::UUID_V7], methods: [Request::METHOD_GET])]
+    public function show(Home $home): Response
+    {
+        return $this->render('home/show.html.twig', [
+            'home' => $home,
         ]);
     }
 }
