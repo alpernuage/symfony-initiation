0ac5ed7b68da4686cfaa10ac25c00a02c1fd1907
Alper AKBULUT <50120472+alpernuage@users.noreply.github.com>
2023-06-22 17:53:15 +0200

    feat(api platform): install api platform (#94)
    * feat(api platform): install api platform
    
    * feat(entities): add ApiResource php attribute on entities
    
    * feat(api platform): reinstall api platform that has been lost with develop branch merge
    
    * refactor(api platform): reinstall api platform
    
    * refactor(api platform): reinstall api platform after upgrading symfony in an other PR
    
    * refactor(api platform): reinstall api platform after upgrading symfony in an other PR
    
    * refactor(entities): remove ApiResource from entities

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
