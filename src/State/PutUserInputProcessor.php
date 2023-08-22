f5466e457ffb8a89b24b2e64247861630f5f7f5f
alper <alperakbulut@gmail.com>
2023-08-23 00:06:06 +0200

    fix(api constraint): fix unique email constraint on api

--- src/State/PutUserInputProcessor.php
+++ src/State/PutUserInputProcessor.php
@@ -11,6 +11,7 @@ use App\Domain\User\Api\PutUserInput;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Uid\UuidV7;
 
 final class PutUserInputProcessor implements ProcessorInterface
@@ -31,7 +32,16 @@ final class PutUserInputProcessor implements ProcessorInterface
         /** @var PutUserInput $data */
         $user->setFirstName($data->firstName);
         $user->setLastName($data->lastName);
-        $user->setEmail($data->email);
+
+        if ($data->email !== $user->getEmail() && $data->email !== null) {
+            $existingUser = $this->userRepository->findOneBy(['email' => $data->email]);
+
+            if ($existingUser) {
+                throw new UnprocessableEntityHttpException('Email already used.');
+            }
+
+            $user->setEmail($data->email);
+        }
 
         $this->entityManager->flush();
 
