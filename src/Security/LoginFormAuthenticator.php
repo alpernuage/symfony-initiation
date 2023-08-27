ae3cf776b6ba37fa27410e2386b591c0d6b7e195
alper <alperakbulut@gmail.com>
2023-08-27 18:00:02 +0200

    refactor(/loginformauthenticator): use AbstractLoginFormAuthenticator to simplify the process

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,33 +3,23 @@
 namespace App\Security;
 
 use App\Repository\SecurityUserRepository;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     public function __construct(private SecurityUserRepository $securityUserRepository, private RouterInterface $router)
     {
-    }
-
-    public function supports(Request $request): ?bool
-    {
-        $locale = $request->attributes->get('_locale');
-
-        return $request->getPathInfo() === '/' . $locale . '/login' && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): Passport
@@ -63,27 +53,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
         return new RedirectResponse($url);
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    protected function getLoginUrl(Request $request): string
     {
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-        $locale = $request->attributes->get('_locale');
-        $url = $this->router->generate('app_login', ['_locale' => $locale]);
-
-        return new RedirectResponse($url);
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        $locale = $request->attributes->get('_locale');
-        $url = $this->router->generate('app_login', ['_locale' => $locale]);
-
-        return new RedirectResponse($url);
-        /*
-         * If you would like this class to control what happens when an anonymous user accesses a
-         * protected page (e.g. redirect to /login), uncomment this method and make this class
-         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-         *
-         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-         */
+        return $this->router->generate('app_login', ['_locale' => $request->attributes->get('_locale')]);
     }
 }
