9698f6a515868a6b6801dc4d509002320c898c99
alper <alperakbulut@gmail.com>
2023-08-30 15:13:35 +0200

    feat(login page): add last user name and csrf token config

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,7 @@ class SecurityController extends AbstractController
     {
         return $this->render('security/login.html.twig', [
             'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername(),
         ]);
     }
 
